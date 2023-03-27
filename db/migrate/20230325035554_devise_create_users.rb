32d2426be727f3a538ec3feb82f70c4087d19684
daisuke <jinjinjajajaja@gmail.com>
2023-03-27 13:56:27 +0000

    06 use_css_framework

--- db/migrate/20230325035554_devise_create_users.rb
+++ db/migrate/20230325035554_devise_create_users.rb
@@ -3,6 +3,10 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+     t.string :name,              null: false
+     t.string :postal_code,              null: false
+     t.string :address,              null: false
+     t.string :phone,              null: false
       
       ## Database authenticatable
       t.string :email,              null: false, default: ""
@@ -16,11 +20,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.datetime :remember_created_at
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       ## Confirmable
       t.string   :confirmation_token
--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,10 @@ ActiveRecord::Schema.define(version: 2023_03_25_035554) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "phone", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
